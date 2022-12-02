ae66e6d29fb6d9fac4d2ab89a02fabf55c468bae
didoolaeghe <diane.doolaeghe@gmail.com>
2022-12-02 14:33:04 +0100

    add data, python functions and notebooks

---
+++ 02-python_functions/utils_fop.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# Diane Doolaeghe: 03/05/2022
+
+# Functions to calcultate fop from a fracture dataset, with open and sealed informations
+
+
+
+import sys, os, math, numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats
+
+
+
+def fop(data, column, **kwargs):  
+    nbin = kwargs.get('bin_number',100)# number of bins
+    bin_min = kwargs.get('min', data[column].min())
+    bin_max = kwargs.get('max', data[column].max())
+
+    bins = np.linspace(bin_min, bin_max, num = nbin, endpoint = True)
+    bins_inf = bins[:-1]
+    bins_sup = bins[1:]
+
+
+    fop32 = []
+    N_bin = []
+    N_bin_norm = []
+    bin_mean =[]
+    for i in range(len(bins_sup)):
+        data_select = data.loc[(data[column] <= bins_sup[i]) & (data[column] > bins_inf[i])].copy()
+
+        _ndata=data_select.shape[0]
+        if _ndata>0:
+            #angle correction
+            uncertainty = 5  
+            max_f = 1/math.sin(uncertainty*math.pi/180)
+            factor = 1/(np.sin(data_select['ALPHA(degrees)']*math.pi/180))
+            factor[factor > max_f] = max_f
+            data_select['factor']=factor
+
+
+            data_select_open = data_select.loc[data_select['FRACT_INTERPRET'].isin(['Open','Partly open'])]
+
+            N_bin.append(_ndata)
+            N_bin_norm.append(_ndata/(bins_sup[i]-bins_inf[i]))#/(bins_sup[i]-bins_inf[i]))#*data.shape[0])
+            bin_mean.append(data_select[column].mean())
+
+            _fop32=data_select_open['factor'].sum()/data_select['factor'].sum()
+            fop32.append(_fop32)
+        else:
+            bin_mean.append((bins_sup[i]+bins_inf[i])/2)
+            fop32.append(np.nan)
+            N_bin.append(np.nan)
+            N_bin_norm.append(np.nan)
+
+    data_save = pd.DataFrame()
+    data_save[column] = np.array(bin_mean)
+    data_save['min'] = bins_inf
+    data_save['max'] = bins_sup
+    data_save['fop32'] = fop32
+    data_save['N'] = N_bin
+    data_save['N_norm'] = N_bin_norm
+
+    return data_save
+
+def fop2D(data, column1, column2,  **kwargs):  
+    nbin1 = kwargs.get('bin_number1',50)# number of bins
+    nbin2 = kwargs.get('bin_number2',50)# number of bins
+    bin_min1 = kwargs.get('min1', data[column1].min())
+    bin_max1 = kwargs.get('max1', data[column1].max())
+    bin_min2 = kwargs.get('min2', data[column2].min())
+    bin_max2 = kwargs.get('max2', data[column2].max())
+
+    #angle correction
+    uncertainty = 5  
+    max_f = 1/math.sin(uncertainty*math.pi/180)
+    factor = 1/(np.sin(data['ALPHA(degrees)']*math.pi/180))
+    factor[factor > max_f] = max_f
+    data['factor']=factor
+
+    bins1 = np.linspace(bin_min1, bin_max1, num = nbin1, endpoint = True)
+    bins2 = np.linspace(bin_min2, bin_max2, num = nbin2, endpoint = True)
+
+    data_open = data.loc[data['FRACT_INTERPRET'].isin(['Open','Partly open'])]
+
+    h1, edges1, edges2 = np.histogram2d(data[column1], data[column2], bins=(bins1, bins2), weights = data['factor'])
+    h2, edges1, edges2 = np.histogram2d(data_open[column1], data_open[column2], bins=(bins1, bins2), weights = data_open['factor'])
+    h = h2 / h1
+
+    return h, edges1, edges2
---
+++ 02-python_functions/utils_stress.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Diane Doolaeghe: 03/05/2022
+
+import numpy as np
+from math import *
+import sys
+
+
+
+
+def stress_tensor(sigma_H,trend_H,plunge_H, sigma_h,trend_h,plunge_h, sigma_V,trend_V,plunge_V):
+    #principal components tensor:
+    Tp = np.array([[sigma_H,0,0],[0,sigma_h,0],[0,0,sigma_V]])
+    # x,y,z direction for each components
+    dir_H = np.array([np.cos(plunge_H*pi/180)*np.sin(trend_H*pi/180),np.cos(plunge_H*pi/180)*np.cos(trend_H*pi/180),-np.sin(plunge_H*pi/180)])
+    dir_h = np.array([np.cos(plunge_h*pi/180)*np.sin(trend_h*pi/180),np.cos(plunge_h*pi/180)*np.cos(trend_h*pi/180),-np.sin(plunge_h*pi/180)])
+    #vertical stress must be pointing downward so that axis are orthonormal, we put a minus.
+    dir_V = -np.array([np.cos(plunge_V*pi/180)*np.sin(trend_V*pi/180),np.cos(plunge_V*pi/180)*np.cos(trend_V*pi/180),-np.sin(plunge_V*pi/180)]) 
+    #check orthogonality
+    if (abs(np.dot(dir_H, dir_h) > 10**-14)):
+        raise Exception("Your principal tensor is not orthogonal")
+    if (abs(np.dot(dir_h, dir_V) > 10**-14)):
+        print(abs(np.dot(dir_h, dir_V)))
+        raise Exception("Your principal tensor is not orthogonal")
+    if (abs(np.dot(dir_H, dir_V) > 10**-14)):
+        raise Exception("Your principal tensor is not orthogonal")
+    # normalize directions
+    dir_H = dir_H/np.linalg.norm(dir_H)
+    dir_h = dir_h/np.linalg.norm(dir_h)
+    dir_V = dir_V/np.linalg.norm(dir_V)
+    #rotation matrix
+    R_mat = np.transpose(np.array([dir_H,dir_h,dir_V]))
+    #stress tensor in x,y,z directions
+    T = np.dot(np.dot(R_mat,Tp),np.transpose(R_mat))  #R_mat.Tp.R_mat_T
+    return T
+
+
+def get_fracture_normale(dip,dipd):
+    dip = dip*pi/180
+    dipd = dipd*pi/180
+    x = np.sin(dip)*np.sin(dipd) 
+    y = np.sin(dip)*np.cos(dipd) 
+    z = np.cos(dip)
+    normale = np.array([x,y,z])
+    return normale
+
+
